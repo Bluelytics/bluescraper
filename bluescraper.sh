8fdbd0e6974be818a60d5bc992c31caec129e059
Pablo Seibelt <pabloseibelt@sicarul.com>
2015-08-24 18:03:17 -0300

    Disable dolarblue.net

--- bluescraper.sh
+++ bluescraper.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 . $DIR/bin/activate
-python $DIR/bluescrape.py
+# python $DIR/bluescrape.py ## -- DolarBlue.net is stuck
 python $DIR/ambitoscrape.py
 python $DIR/lanacionscrape.py
 python $DIR/ofiscrape.py

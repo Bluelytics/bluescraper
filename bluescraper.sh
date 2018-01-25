08cd2c1f72e1aa5950450f8a36b1f120d0e2ce75
Pablo Seibelt <pabloseibelt@gmail.com>
2018-01-24 21:25:30 -0300

    Only from ambito

--- bluescraper.sh
+++ bluescraper.sh
@@ -6,6 +6,6 @@ cd $DIR
 . $DIR/bin/activate
 # python $DIR/bluescrape.py ## -- DolarBlue.net is stuck
 python $DIR/ambitoscrape.py
-python $DIR/lanacionscrape.py
+#python $DIR/lanacionscrape.py
 python $DIR/ofiscrape.py
-python $DIR/elcronistascrape.py
+#python $DIR/elcronistascrape.py

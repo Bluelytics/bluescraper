52f050a93139e471d79349e10421c9ac4e1e9bf8
Pablo Seibelt <pabloseibelt@gmail.com>
2020-12-28 12:08:16 -0300

    Change email ntoif

--- src/buffer.py
+++ src/buffer.py
@@ -16,10 +16,9 @@ def send_post(social_network, msg, img):
 
     if r.status_code != 200:
         print(r.text)
-
-    sns = boto3.client("sns")
-    sns.publish(TopicArn='arn:aws:sns:us-west-2:151654911502:mailPablo', 
-            Message="{}\n\nhttps://{}.s3-us-west-2.amazonaws.com/{}".format(msg, os.environ['S3_BUCKET'], img), 
-            Subject="New update available for {}".format(social_network))
+        sns = boto3.client("sns")
+        sns.publish(TopicArn='arn:aws:sns:us-west-2:151654911502:mailPablo', 
+                Message="{}\n\nhttps://{}.s3-us-west-2.amazonaws.com/{}".format(msg, os.environ['S3_BUCKET'], img), 
+                Subject="New update available for {}".format(social_network))
 
     return r.status_code

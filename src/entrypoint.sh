a350ff44c758f896879accb61a2b391e09d0e222
Pablo Seibelt <pabloseibelt@gmail.com>
2022-07-28 23:45:35 -0300

    Add print

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+echo "Running scrapers..."
 # Scrape
 python scrape_ambito.py
 python scrape_lanacion.py
 python scrape_oficial.py
 python scrape_cronista.py
 
+echo "Refreshing dollar..."
 # Refresh last dolar
 python refresh_dolar.py
 
+echo "Posting updates..."
 # Alerts/Updates
 python alerts.py
 python updates.py
